8303dfb7b611d3ac9eb1c9fb05f7eef64abdb4be
Julia Prestes <71856252+juliaprestes@users.noreply.github.com>
2021-10-26 21:01:18 -0300

    Add files via upload

---
+++ ByteBank/LeitorDeArquivos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ByteBank
+{
+    public class LeitorDeArquivos : IDisposable
+    {
+        public string Arquivo { get; }
+        public LeitorDeArquivos(string arquivo)
+        {
+            Arquivo = arquivo;
+            int divisor = 0;
+            int resultado = 10 / divisor;
+
+            throw new FileNotFoundException();
+            Console.WriteLine("Abrindo arquivo: " + arquivo);
+        }
+        public string LerProximaLinha()
+        {
+            Console.WriteLine("Lendo linha...");
+
+            throw new IOException();
+            return "linha do arquivo";
+        }
+      
+        public void Dispose()
+        {
+            Console.WriteLine("Fechando arquivo.");
+        }
+    }
+}
